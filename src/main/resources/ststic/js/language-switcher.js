d19bbef924d13be20af3f0321c314eb14f492db3
gamblerLt <rimas.spakauskas@outlook.com>
2023-05-23 20:03:06 +0300

    pizza-ui

--- src/main/resources/ststic/js/language-switcher.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('#language-switcher').change(function(){
-        const lang = $('#language-switcher').val();
-
-        if(lang && lang.length > 0) {
-            window.location.replace(`?lang=${lang}`);
-        }
-    });
-});
