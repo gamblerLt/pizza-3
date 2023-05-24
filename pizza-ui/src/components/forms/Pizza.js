fecda64052fc2f54582bbcfe8130925ca5644b5e
gamblerLt <rimas.spakauskas@outlook.com>
2023-05-24 20:59:30 +0300

    formtextinput

--- pizza-ui/src/components/forms/Pizza.js
+++ pizza-ui/src/components/forms/Pizza.js
@@ -1,26 +1,38 @@
 import {ErrorMessage, Field, Form, Formik} from "formik";
-import {CircularProgress, Stack, TextField} from "@mui/material";
+import {Button, CircularProgress, FormControl, FormHelperText, Stack, TextField, Typography} from "@mui/material";
 import * as Yup from 'yup';
+import FormTextInput from "./FormTextInput";
 
 const productValidationSchema = Yup.object().shape(
     {
         name: Yup.string()
             .min(5, 'Name must be more then 5 symbols')
             .max(10, 'Name must be less then 10 symbols')
-            .required(),
-
+            .required('Name is required'),
         category: Yup.string()
-            .min(5, 'Kategorija must be more then 5 symbols')
-            .max(10, 'Kategorija must be less then 10 symbols')
-            .required()
+            .required('Category is required'),
+        description: Yup.string()
+            .required('Description is required'),
+        quantity: Yup.number()
+            .typeError('Quantity must be a number')
+            .positive('Quantity must be bigger then 0')
+            .required('Quantity is required'),
+        price: Yup.number()
+            .typeError('Price must be a number')
+            .positive('Price must be bigger then 0')
+            .required('Price is required')
     }
 )
+
 const Pizza = () => (
 
     <Formik
         initialValues={{
-            title: '',
-            category: ''
+            name: '',
+            category: '',
+            description: '',
+            quantity: '',
+            price: ''
         }}
 
         onSubmit={(values, helpers) => {
@@ -32,28 +44,33 @@ const Pizza = () => (
                 helpers.resetForm();
             }, 5000);
         }}
+
         validationSchema={productValidationSchema}
     >
         {props => (
             <Form>
                 <Stack spacing={2} direction="column">
-                    <Field id="name"
-                           name="name"
-                           as={TextField}
-                           label="Product name"
-                           variant="outlined"/>
-                    <ErrorMessage name="name" component="div"/>
-
-                    <Field id="category"
-                           name="category"
-                           as={TextField}
-                           label="Category"
-                           variant="outlined"/>
-                    <ErrorMessage name="category" component="div"/>
+                    <FormTextInput error={props.touched.name && !!props.errors.name}
+                                   name="name"
+                                   label="Product name"/>
+                    <FormTextInput error={props.touched.category && !!props.errors.category}
+                                   name="category"
+                                   label="Product category"/>
+                    <FormTextInput error={props.touched.description && !!props.errors.description}
+                                   name="description"
+                                   label="Product description"/>
+                    <FormTextInput error={props.touched.quantity && !!props.errors.quantity}
+                                   name="quantity"
+                                   label="Product quantity"/>
+                    <FormTextInput error={props.touched.price && !!props.errors.price}
+                                   name="price"
+                                   label="Product price"/>
                 </Stack>
-                {
-                    props.isSubmitting ? <CircularProgress/> : <button type="submit">Submit</button>
-                }
+                <Typography sx={{textAlign: 'right', mt: 2}}>
+                    {
+                        props.isSubmitting ? <CircularProgress/> : <Button variant="outlined" type="submit">Save</Button>
+                    }
+                </Typography>
             </Form>
         )
         }
