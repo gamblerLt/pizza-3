91f9ed26e1573000696aa32955ecddd381424cbb
gamblerLt <rimas.spakauskas@outlook.com>
2023-06-01 19:35:41 +0300

    Registration works good

--- pizza-ui/src/components/api/userApi.js
+++ pizza-ui/src/components/api/userApi.js
@@ -8,27 +8,3 @@ export {
     getUserById
 }
 
-
-
-
-
-/*
-
-
-import HTTP from "./index";
-
-const getPizzas =  () => HTTP.get('/pizzas');
-const savePizza = (pizza) => HTTP.post('/pizzas', pizza);
-
-const updatePizza = (id, pizza) => HTTP.put(`/pizzas/${id}`, pizza);
-
-const deletePizza = (id, pizza) => HTTP.delete(`/pizzas/${id}`, pizza);
-const getPizzaById = (id) => HTTP.get(`/pizzas/${id}`);
-
-export {
-    getPizzas,
-    savePizza,
-    updatePizza,
-    deletePizza,
-    getPizzaById
-}*/
--- pizza-ui/src/components/forms/UserRegistration.js
+++ pizza-ui/src/components/forms/UserRegistration.js
@@ -1,40 +1,29 @@
-import React from "react";
-import {Form, Formik, Field} from "formik";
+import React, { useEffect, useState } from "react";
+import { Form, Formik } from "formik";
 import {
     Alert,
     Button,
     CircularProgress,
     Stack,
     Typography,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Input
 } from "@mui/material";
 import * as Yup from "yup";
 import FormTextInput from "./FormTextInput";
 
-import {createUser, getUserById} from "../api/userApi";
-
-
-import {useEffect, useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
-import axios from "axios";
-
-f
-
+import { createUser, getUserById } from "../api/userApi";
+import { useParams } from "react-router-dom";
 
 const userValidationSchema = Yup.object().shape({
-    username: Yup.string()
+    name: Yup.string()
         .min(4, "Username must be more than 4 characters")
         .max(10, "Username must be less than 10 characters")
         .required("Username is required"),
-    email: Yup.string().email("Invalid email").required("Email is required"),
     phone: Yup.string()
         .matches(/^\+?\d{1,4}\d{5,}$/, "Invalid phone number")
         .min(5, "Phone number must be longer than 5 digits")
         .required("Phone number is required"),
+    address: Yup.string().required('Address is required'),
+    email: Yup.string().email("Invalid email").required("Email is required"),
     password: Yup.string()
         .min(5, "Password must be more than 5 characters")
         .max(10, "Password must be less than 10 characters")
@@ -44,131 +33,126 @@ const userValidationSchema = Yup.object().shape({
         .required("Password confirmation is required"),
 });
 
-const UserRegistration = () => (
-const [notification, setNotification] = useState({isVisible: false});
-const {id} = useParams();
-const [user, setUser] = useState({
-    username: '',
-    email: '',
-    phone: '',
-    password: '',
-    passwordConfirmation: ''
-});
-const [loading, setLoading] = useState(true);
-const navigation = useNavigate();
+const UserRegistration = () => {
+    const [notification, setNotification] = useState({
+        isVisible: false,
+        message: "",
+        severity: "success",
+    });
+    const { id } = useParams();
+    const [user, setUser] = useState({
+        name: "",
+        phone: "",
+        address: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+    });
+    const [loading, setLoading] = useState(true);
+    //const navigation = useNavigate();
 
-useEffect(() => {
-    if (!id) {
-        setLoading(false);
-        return;
-    }
-    getUserById(id)
-        .then(({data}) => setUser(data))
-        .catch((err) => console.log(err))
-        .finally(() => setLoading(false));
-}, []);
-const onCreateUser = (values, helper) => {
-    saveUser(values)
-        .then((response) => {
+    useEffect(() => {
+        if (!id) {
+            setLoading(false);
+            return;
+        }
+        getUserById(id)
+            .then(({ data }) => setUser(data))
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+    }, [id]);
+
+    const onFormSubmit = (values, helper) => {
+        onCreateUser(values, helper);
+    };
+
+    const onCreateUser = (values, helper) => {
+        createUser(values)
+            .then(() => {
                 helper.resetForm();
-                setNotification({isVisible: true, message: 'User registration success', severity: 'success'});
-            }
-        )
-        .catch((error) => {
-                setNotification({isVisible: true, message: 'User registration NOT success', severity: 'error'});
+                setNotification({
+                    isVisible: true,
+                    message: "User registration success",
+                    severity: "success",
+                });
+            })
+            .catch((error) => {
+                setNotification({
+                    isVisible: true,
+                    message: "User registration NOT success",
+                    severity: "error",
+                });
                 console.log(error);
-            }
-        )
-        .finally(() => helper.setSubmitting(false));
-};
-return (
-<>
-{
-    loading...........
+            })
+            .finally(() => helper.setSubmitting(false));
+    };
 
-
-    </>
-
-
-
-    <Formik
-    initialValues={{
-    username: "",
-    email: "",
-    phone: "",
-    password: "",
-    passwordConfirmation: "",
-}}
-onSubmit={(values, helpers) => {
-    createUser(values)
-        .then((response) => {
-            console.log("User created successfully:", response.data);
-            helpers.setSubmitting(false);
-            helpers.resetForm({
-                username: "",
-                email: "",
-                phone: "",
-                password: "",
-                passwordConfirmation: "",
-            });
-        })
-        .catch((error) => {
-            console.error("Error creating user:", error);
-            helpers.setSubmitting(false);
-        });
-}}
-validationSchema={userValidationSchema}
->
-{(props) => (
-    <Form>
-        <Stack spacing={2} direction="column">
-            <Typography variant="h6" component="h6">
-                User Registration
-            </Typography>
-            <FormTextInput
-                error={props.touched.username && !!props.errors.username}
-                name="username"
-                label="Username"
-            />
-            <FormTextInput
-                error={props.touched.email && !!props.errors.email}
-                name="email"
-                label="Email"
-            />
-            <FormTextInput
-                error={props.touched.phone && !!props.errors.phone}
-                name="phone"
-                label="Phone (e.g., +370...)"
-            />
-            <FormTextInput
-                error={props.touched.password && !!props.errors.password}
-                name="password"
-                label="Password"
-                type="password"
-            />
-            <FormTextInput
-                error={
-                    props.touched.passwordConfirmation &&
-                    !!props.errors.passwordConfirmation
-                }
-                name="passwordConfirmation"
-                label="Confirm Password"
-                type="password"
-            />
-        </Stack>
-        <Typography sx={{textAlign: "right", mt: 2}}>
-            {props.isSubmitting ? (
-                <CircularProgress/>
+    return (
+        <>
+            {loading ? (
+                <CircularProgress />
             ) : (
-                <Button variant="outlined" type="submit">
-                    Register
-                </Button>
+                <Formik
+                    initialValues={user}
+                    onSubmit={onFormSubmit}
+                    validationSchema={userValidationSchema}
+                >
+                    {(props) => (
+                        <Form>
+                            <Stack spacing={2} direction="column">
+                                {notification.isVisible && (
+                                    <Alert severity={notification.severity}>
+                                        {notification.message}
+                                    </Alert>
+                                )}
+                                <Typography variant="h6">Create User</Typography>
+                                <FormTextInput
+                                    error={props.touched.name && !!props.errors.name}
+                                    name="name"
+                                    label="Name"
+                                />
+                                <FormTextInput
+                                    error={props.touched.phone && !!props.errors.phone}
+                                    name="phone"
+                                    label="Phone number (e.g., +370...)"
+                                />
+                                <FormTextInput
+                                    error={props.touched.address && !!props.errors.address}
+                                    name="address"
+                                    label="Address"
+                                />
+                                <FormTextInput
+                                    error={props.touched.email && !!props.errors.email}
+                                    name="email"
+                                    label="Email"
+                                />
+                                <FormTextInput
+                                    error={props.touched.password && !!props.errors.password}
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                />
+                                <FormTextInput
+                                    error={
+                                        props.touched.passwordConfirmation &&
+                                        !!props.errors.passwordConfirmation
+                                    }
+                                    name="passwordConfirmation"
+                                    label="Confirm password"
+                                    type="password"
+                                />
+                            </Stack>
+                            <Typography sx={{ textAlign: "right", mt: 2 }}>
+                                <Button variant="outlined" type="submit">
+                                    Register
+                                </Button>
+                            </Typography>
+                        </Form>
+                    )}
+                </Formik>
             )}
-        </Typography>
-    </Form>
-)}
-</Formik>
-)
-;
+        </>
+    );
+};
 
 export default UserRegistration;
