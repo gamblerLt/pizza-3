4e94e3c8f0c2aed914d0629ca84563318641c7ad
gamblerLt <rimas.spakauskas@outlook.com>
2023-06-13 16:54:52 +0300

    rolled back

--- pizza-ui/src/components/header/Header.js
+++ pizza-ui/src/components/header/Header.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
-import {AppBar, Button, Divider, ListItemIcon, Menu, MenuItem, Toolbar, Typography} from "@mui/material";
-import { NavLink } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { useSelector, useDispatch } from "react-redux";
-import { removeUser } from "../../store/slices/userSlice";
+import {
+    AppBar,
+    Avatar,
+    Button,
+    Divider,
+    IconButton,
+    Link,
+    ListItemIcon, Menu,
+    Toolbar,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import MenuItem from "./MenuItem";
 import logo from "./img/pizza-logo-long.png";
+import {useTranslation} from "react-i18next";
 import LanguageSwitcher from "../switcher/LanguageSwitcher";
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {removeUser} from "../../store/slices/userSlice";
 import {Logout, Settings} from "@mui/icons-material";
 
+
 const Header = () => {
-    const user = useSelector((state) => state.user);
+
+
+   // const user = useSelector(state => state.user.user);
+    const user = useSelector(state => state.user);
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
     const dispatch = useDispatch();
@@ -24,125 +40,125 @@ const Header = () => {
 
     const onLogout = () => {
         dispatch(removeUser());
-    };
+    }
 
-    const { t } = useTranslation("header");
+    const {t} = useTranslation('header');
+
 
     return (
-        <AppBar position="static" color="default" elevation={0}>
-            <Toolbar>
+        <AppBar
+            position="static"
+            color="default"
+            elevation={0}
+            sx={{borderBottom: (theme) => `1px solid ${theme.palette.divider}`}}>
+
+            <Toolbar sx={{flexWrap: "wrap", justifyContent: "center"}}>
                 <Typography variant="h6" color="inherit" noWrap>
-                    <NavLink to="/" style={{ textDecoration: "none" }}>
+                    <Link component={NavLink} to="/" underline="none">
                         <img
                             src={logo}
                             alt="Logo"
-                            style={{ height: "30px", marginRight: "10px" }}
+                            style={{height: '30px', marginRight: '10px'}}
                         />
-                    </NavLink>
+                    </Link>
                 </Typography>
                 <nav>
-                    <NavLink
-                        to="/"
-                        style={{ textDecoration: "none", marginRight: "10px" }}
-                        activeStyle={{ fontWeight: "bold" }}
-                    >
-                        {t("pizzas")}
-                    </NavLink>
-                    <NavLink
-                        to="/pizzas/create"
-                        style={{ textDecoration: "none", marginRight: "10px" }}
-                        activeStyle={{ fontWeight: "bold" }}
-                    >
-                        {t("create.pizza")}
-                    </NavLink>
-                    <NavLink
-                        to="/users/registration"
-                        style={{ textDecoration: "none", marginRight: "10px" }}
-                        activeStyle={{ fontWeight: "bold" }}
-                    >
-                        {t("create.user")}
-                    </NavLink>
+                    <MenuItem path="/" name={t("pizzas")}/>
+                    <MenuItem path="/pizzas/create" name={t("create.pizza")}/>
+                    <MenuItem path="/users/registration" name={t("create.user")}/>
                 </nav>
 
-                {user ?
-                    <>
-                        <Button
-                            onClick={handleClick}
-                            size="small"
-                            sx={{ mx: 2 }}
-                            aria-controls={open ? "account-menu" : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? "true" : undefined}
-                        >
-                            {user.name}
-                        </Button>
-                        <Menu
-                            anchorEl={anchorEl}
-                            id="account-menu"
-                            open={open}
-                            onClose={handleClose}
-                            onClick={handleClose}
-                            PaperProps={{
-                                elevation: 0,
-                                sx: {
-                                    overflow: "visible",
-                                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                                    mt: 1.5,
-                                    "&:before": {
-                                        content: '""',
-                                        display: "block",
-                                        position: "absolute",
-                                        top: 0,
-                                        right: 14,
-                                        width: 10,
-                                        height: 10,
-                                        bgcolor: "background.paper",
-                                        transform: "translateY(-50%) rotate(45deg)",
-                                        zIndex: 0,
+               {user ?
+                        <>
+                            <Tooltip title="Account">
+                                <IconButton onClick={handleClick} size="small" sx={{mx: 2}}
+                                            aria-controls={open ? 'account-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}>
+                                    <Avatar sx={{width: 32, height: 32}}></Avatar>
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                anchorEl={anchorEl}
+                                id="account-menu"
+                                open={open}
+                                onClose={handleClose}
+                                onClick={handleClose}
+                                PaperProps={{
+                                    elevation: 0,
+                                    sx: {
+                                        overflow: 'visible',
+                                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                        mt: 1.5,
+                                        '& .MuiAvatar-root': {
+                                            width: 32,
+                                            height: 32,
+                                            ml: -0.5,
+                                            mr: 1,
+                                        },
+                                        '&:before': {
+                                            content: '""',
+                                            display: 'block',
+                                            position: 'absolute',
+                                            top: 0,
+                                            right: 14,
+                                            width: 10,
+                                            height: 10,
+                                            bgcolor: 'background.paper',
+                                            transform: 'translateY(-50%) rotate(45deg)',
+                                            zIndex: 0,
+                                        },
                                     },
-                                },
-                            }}
-                            transformOrigin={{ horizontal: "right", vertical: "top" }}
-                            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-                        >
-                            <MenuItem>
-                                {user.name}
-                            </MenuItem>
-                            <Divider />
-                            <MenuItem onClick={handleClose}>
-                                <ListItemIcon>
-                                    <Settings
-                                        fontSize="small"
-                                        onClick={(event) => event.stopPropagation()}
-                                    />
-                                </ListItemIcon>
-                                Login
-                            </MenuItem>
-                            <MenuItem onClick={onLogout}>
-                                <ListItemIcon>
-                                    <Logout
-                                        fontSize="small"
-                                        onClick={(event) => event.stopPropagation()}
-                                    />
-                                </ListItemIcon>
-                                Logout
-                            </MenuItem>
-                        </Menu>
-                    </>
-                 :
-                    <Button
-                        variant="outlined"
-                        sx={{ my: 1, mx: 1.5 }}
-                        component={NavLink}
-                        to="users/login"
-                    >
-                        Login
-                    </Button>
+                                }}
+                                transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                                anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}>
+                                <MenuItem>
+                                    <Avatar/> {user.fullName}
+                                </MenuItem>
+                                <Divider/>
+                                {/*<MenuItem onClick={handleClose}>
+                                    <ListItemIcon>
+                                        <Settings fontSize="small"/>
+                                    </ListItemIcon>
+                                    Settings
+                                </MenuItem>
+                                <MenuItem onClick={onLogout}>
+                                    <ListItemIcon>
+                                        <Logout fontSize="small"/>
+                                    </ListItemIcon>
+                                    Logout
+                                </MenuItem>*/}
+                                <MenuItem onClick={handleClose}>
+                                    <ListItemIcon>
+                                        <Settings fontSize="small" onClick={(event) => event.stopPropagation()} />
+                                    </ListItemIcon>
+                                    Settings
+                                </MenuItem>
+                                <MenuItem onClick={onLogout}>
+                                    <ListItemIcon>
+                                        <Logout fontSize="small" onClick={(event) => event.stopPropagation()} />
+                                    </ListItemIcon>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
+                        </>
+                        :
+
+                   <Button
+                       variant="outlined"
+                       sx={{ my: 1, mx: 1.5 }}
+                       component={NavLink}
+                       to="users/login"
+                   >
+                       Login
+                   </Button>
                 }
-                <LanguageSwitcher />
+                <LanguageSwitcher/>
+
             </Toolbar>
         </AppBar>
-    );
-};
+    )
+        ;
+}
 
 export default Header;
